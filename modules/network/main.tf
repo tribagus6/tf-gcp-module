be2793a695570d4075615a50b277c091b48a3816
tribagus6 <tribagus325@gmail.com>
2026-09-23 09:44:06 +0000

    feat(network): output vpc_self_link

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -71,6 +71,10 @@ output "vpc_name" {
   value = google_compute_network.vpc.name
 }
 
+output "vpc_self_link" {
+  value = google_compute_network.vpc.self_link
+}
+
 output "subnet_self_links" {
   value = { for k, v in google_compute_subnetwork.subnet : k => v.self_link }
 }
